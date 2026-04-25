9ce5dc7db3da72566e9eedbb109cc9fc82a701cd
Yousef Zaqout <zaqoutyousef@gmail.com>
2026-04-25 18:59:12 +0300

    Add Spring Boot application class

---
+++ src/main/java/com/assignment/communicationapp/CommunicationAppApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.communicationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunicationAppApplication.class, args);
+    }
+
+}
